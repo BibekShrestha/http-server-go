78195253a59763d6a1966ba1c976ef3a708fcfa4
Bibek Shrestha <harry.stha4742@gmail.com>
2024-05-11 16:24:10 +0545

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -12,16 +12,29 @@ func main() {
 	fmt.Println("Logs from your program will appear here!")
 
 	// Uncomment this block to pass the first stage
-	
+
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
 
-	_, err = l.Accept()
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+		go handleConnection(conn)
+	}
+}
+
+func handleConnection(conn net.Conn) {
+	response := fmt.Sprintf("%s %s %s%s%s", HTTPVersion, Response_200, STATUS_OK, EOL, EOL)
+	fmt.Println("Writing : ", response)
+	_, err := conn.Write([]byte(response))
+	//_, err := conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
 	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+		fmt.Println("Error when writing response: ", err)
 	}
 }
---
+++ app/vars.go
@@ -0,0 +1,8 @@
+package main
+
+var (
+	HTTPVersion  = "HTTP/1.1"
+	EOL          = "\r\n"
+	STATUS_OK    = "OK"
+	Response_200 = "200"
+)
